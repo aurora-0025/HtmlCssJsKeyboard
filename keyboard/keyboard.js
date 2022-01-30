91664631f3508424a3694d7d69c11337efe5a438
aurora-0025 <ritingeorge.d@gmail.com>
2022-01-30 20:58:01 +0530

    fixed sound linking

--- keyboard/keyboard.js
+++ keyboard/keyboard.js
@@ -13,16 +13,16 @@ window.addEventListener('keydown', function (e) {
     let keyDown = document.getElementById(`${e.code}`)
     if(e.repeat) return;
     if(e.code == "Enter"){
-        return new Audio('../sounds/laptop_notebook_return_or_enter_key_press.mp3').play();
+        return new Audio('sounds/laptop_notebook_return_or_enter_key_press.mp3').play();
     }
     else if(e.code == "Backspace"){
-        new Audio('../sounds/laptop_notebook_delete_key_press.mp3').play();
+        new Audio('sounds/laptop_notebook_delete_key_press.mp3').play();
     }
     else if(e.code == "Space"){
-        new Audio('../sounds/laptop_notebook_spacebar_press.mp3').play();
+        new Audio('sounds/laptop_notebook_spacebar_press.mp3').play();
     }
     else{
-        new Audio('../sounds/laptop_notebook_key_press.mp3').play();
+        new Audio('sounds/laptop_notebook_key_press.mp3').play();
     }
     if(keyDown==null){
         return console.log('key pressed which is not in this keyboard');
