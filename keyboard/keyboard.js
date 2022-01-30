c99e8df737803a20ab54729686e4f11f150759f7
aurora-0025 <ritingeorge.d@gmail.com>
2022-01-30 19:43:43 +0530

    Initialize

--- keyboard/keyboard.js
+++ keyboard/keyboard.js
@@ -11,14 +11,31 @@ keys.forEach((key) => {
 
 window.addEventListener('keydown', function (e) {
     let keyDown = document.getElementById(`${e.code}`)
-
+    if(e.repeat) return;
+    if(e.code == "Enter"){
+        return new Audio('../sounds/laptop_notebook_return_or_enter_key_press.mp3').play();
+    }
+    else if(e.code == "Backspace"){
+        new Audio('../sounds/laptop_notebook_delete_key_press.mp3').play();
+    }
+    else if(e.code == "Space"){
+        new Audio('../sounds/laptop_notebook_spacebar_press.mp3').play();
+    }
+    else{
+        new Audio('../sounds/laptop_notebook_key_press.mp3').play();
+    }
+    if(keyDown==null){
+        return console.log('key pressed which is not in this keyboard');
+    }
     keyDown.classList.add('active')
 
 })
 
 window.addEventListener('keyup', function (e) {
     let keyUp = document.getElementById(`${e.code}`)
-
+    if(keyUp==null){
+        return console.log('key up which is not in this keyboard');
+    }
     keyUp.classList.remove('active')
     keyUp.classList.add('remove')
     setTimeout(() => {
